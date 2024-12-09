340ba0c6d2b05ad6f8c0f1a8f8d571043a98d7f4
20jacksond <dlj3@rice.edu>
2024-12-08 22:27:10 -0800

    Fixed publishing, still correcting the inverse kinematics for the momentum trajectory.

---
+++ launch/pixar_m.launch.py
@@ -0,0 +1,98 @@
+"""Launch the momentum test
+
+This should start
+  1) RVIZ, ready to view the robot
+  2) The robot_state_publisher to broadcast the robot model
+  3) The HW7 P3 code to move the joints
+
+"""
+
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory as pkgdir
+
+from launch                            import LaunchDescription
+from launch.actions                    import DeclareLaunchArgument
+from launch.actions                    import OpaqueFunction
+from launch.actions                    import Shutdown
+from launch.substitutions              import LaunchConfiguration
+from launch_ros.actions                import Node
+
+
+#
+# Generate the Launch Description
+#
+def generate_launch_description():
+
+    ######################################################################
+    # LOCATE FILES
+
+    # Locate the RVIZ configuration file.
+    rvizcfg = os.path.join(pkgdir('pixar'), 'rviz/pixar.rviz')
+
+    # Locate the URDF file
+    lamp_urdf = os.path.join(pkgdir('pixar'), 'urdf/lamp.urdf')
+    scene_urdf = os.path.join(pkgdir('pixar'), 'urdf/scene.urdf')
+
+    # Load the robot's URDF file (XML).
+    with open(lamp_urdf, 'r') as file:
+        lamp_description = file.read()
+
+    with open(scene_urdf, 'r') as file:
+        scene_description = file.read()
+
+    ######################################################################
+    # PREPARE THE LAUNCH ELEMENTS
+
+    # Configure a node for the robot_state_publisher.
+    node_tf = Node(
+        name       = 'tf', 
+        package    = 'pixar',
+        executable = 'broadcaster',
+        output     = 'screen')
+    
+    node_trajectory = Node(
+        name       = 'trajectory', 
+        package    = 'pixar',
+        executable = 'momentum',
+        output     = 'screen')
+
+
+    # Configure a node for the robot_state_publisher.
+    node_robot_state_publisher_lamp = Node(
+        name       = 'robot_state_publisher', 
+        namespace="lamp",
+        package    = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output     = 'screen',
+        parameters = [{'robot_description': lamp_description},
+                      {'publish_frequency': 60.0}])
+    
+    node_robot_state_publisher_scene = Node(
+        name       = 'robot_state_publisher', 
+        namespace="scene",
+        package    = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output     = 'screen',
+        parameters = [{'robot_description': scene_description}])
+    
+    # Configure a node for RVIZ
+    node_rviz = Node(
+        name       = 'rviz', 
+        package    = 'rviz2',
+        executable = 'rviz2',
+        output     = 'screen',
+        arguments  = ['-d', rvizcfg],
+        on_exit    = Shutdown())
+
+
+    ######################################################################
+    # RETURN THE ELEMENTS IN ONE LIST
+    return LaunchDescription([
+        # Start the robot_state_publisher, RVIZ, and the trajectory.
+        node_trajectory,
+        node_robot_state_publisher_lamp,
+        node_robot_state_publisher_scene,
+        node_rviz,
+    ])
--- pixar/momentum_test.py
+++ pixar/momentum_test.py
@@ -10,61 +10,45 @@ from pixar.helpers.GeneratorNode      import GeneratorNode
 from pixar.helpers.TransformHelpers   import *
 from pixar.helpers.TrajectoryUtils    import *
 
+from hw6code.KinematicChain     import KinematicChain
+
 # link length
-l = 0.5
+LENGTH = 0.5
 
 # lamp head radius
-r = 0.25
+RADIUS = 0.25
+
+# link masses
+m1 = 0.1
+m2 = 0.1
+m3 = 0.1
+m4 = 0.1
+m5 = 0.1
 
 #
 #   Lamp Kinematics
 #
-def fkin(q):
-    T_01 = T_from_Rp(Rotz(q[0]), pzero())
-    T_12 = T_from_Rp(Roty(-np.pi/2)@Roty(-q[1]), pxyz(0, 0, l))
-    T_23 = T_from_Rp(Roty(-q[2]), pxyz(l, 0, 0))
-    T_34 = T_from_Rp(Roty(-q[3]), pxyz(l, 0, 0))
-    T_45 = T_from_Rp(Rotx(q[4]), pxyz(l, 0, 0))
-    return [T_01, T_12, T_23, T_34, T_45], T_01 @ T_12 @ T_23 @ T_34 @ T_45
+# def fkin(q):
+#     T_01 = T_from_Rp(Rotz(q[0]), pzero())
+#     T_12 = T_from_Rp(Roty(-np.pi/2)@Roty(-q[1]), pxyz(0, 0, l))
+#     T_23 = T_from_Rp(Roty(-q[2]), pxyz(l, 0, 0))
+#     T_34 = T_from_Rp(Roty(-q[3]), pxyz(l, 0, 0))
+#     T_45 = T_from_Rp(Rotx(q[4]), pxyz(l, 0, 0))
+#     return [T_01, T_12, T_23, T_34, T_45], T_01 @ T_12 @ T_23 @ T_34 @ T_45
 
-def Jac(q):
-    # this is the jacobian calculator for the lamp center of mass, NOT tip
-    # re-evaluates at each position
-    
-    # first we must determine where each link's COM is
-    frames, _ = fkin(q)
-    c1 = frames[0] @ pxyz(0, 0, l/2) #FIXME
-    c2 = frames[0] @ frames[1] @ pxyz(l/2, 0, 0)
-    c3 = frames[0] @ frames[1] @ frames[2] @ pxyz(l/2, 0, 0)
-    c4 = frames[0] @ frames[1] @ frames[2] @ frames[3] @ pxyz(l/2, 0, 0)
-    c5 = frames[0] @ frames[1] @ frames[2] @ frames[3] @ frames[4] @ pxyz(r/2, 0, 0)
-
-    # we also need to find where each joint is
-    p1 = pzero()
-    p2 = frames[0] @ pxyz(l, 0, 0)
-    p3 = frames[0] @ frames[1] @ pxyz(l, 0, 0)
-    p4 = frames[0] @ frames[1] @ frames[2] @ pxyz(l, 0, 0)
-    p5 = frames[0] @ frames[1] @ frames[2] @ frames[3] @ pzero()
-
-    # calculate the columns of the jacobian for the given configuration
-    j1 = np.vstack(cross(nz(), c1 - p1))#, nz())
-    j2 = np.vstack(cross(ny(), c2 - p2))#, ny())
-    j3 = np.vstack(cross(ny(), c3 - p3))#, ny())
-    j4 = np.vstack(cross(ny(), c4 - p4))#, ny())
-    j5 = np.vstack(cross(nz(), c5 - p5))#, nz())
-
-    # complete the jacobian
-    J = np.hstack((j1, j2, j3, j4, j5))
-
-    return J
 #
 #   Trajectory Class
 #
-class Trajectory():
+class Trajectory2():
     # Initialization.
     def __init__(self, node):
         # Initialize the current joint position to the starting
         # position and set the desired orientation to match.
+        self.chain0 = KinematicChain(node, 'base', 'link0', self.jointnames()[0])
+        self.chain1 = KinematicChain(node, 'base', 'link1', self.jointnames()[0:2])
+        self.chain2 = KinematicChain(node, 'base', 'link2', self.jointnames()[0:3])
+        self.chain3 = KinematicChain(node, 'base', 'head', self.jointnames()) # note that j4 and 5 both act on the head
+        # we need each frame for the jacobian calculation above
         self.qd = np.zeros((5,))
         self.Rd = Reye()
 
@@ -75,6 +59,64 @@ class Trajectory():
     def jointnames(self):
         # Return a list of joint names FOR THE EXPECTED URDF!
         return ['theta1', 'theta2', 'theta3', 'theta4', 'theta5']
+    
+    # define the jacobian for kinematic calculations
+    def Jac(self, q):
+        # this is the jacobian calculator for the lamp center of mass, NOT tip
+        # re-evaluates at each position
+        
+        # we can't use the Kinematic Chain class to get the Jacobian because that
+        # returns the one for the tip, not the center of mass
+        
+        # first we must determine where each link's COM is
+        f0 = self.chain0
+        f1 = self.chain1
+        f2 = self.chain2
+        f3 = self.chain3
+        c1 = f0 @ pxyz(0, 0, LENGTH/2) #FIXME
+        c2 = f0 @ f1 @ pxyz(LENGTH, 0, 0)
+        c3 = f0 @ f1 @ f2 @ pxyz(LENGTH/2, 0, 0)
+        c4 = f0 @ f1 @ f2 @ f3 @ pxyz(RADIUS/2, 0, 0)
+
+        # we also need to find where each joint is
+        p1 = pzero()
+        p2 = f0 @ pxyz(LENGTH, 0, 0)
+        p3 = f0 @ f1 @ pxyz(LENGTH, 0, 0)
+        p4 = f0 @ f1 @ f2 @ pxyz(LENGTH, 0, 0)
+        # note that the fifth joint is connected directly to the fourth link
+
+        # calculate the jacobian for each link's center of mass
+        # note that the orientation of the center of mass doesn't really matter
+        J1 = np.zeros((5, 3))
+        J1[:, 0] = np.vstack(cross(nz(), c1 - p1), nz())
+        
+        J2 = np.zeros((5, 3))
+        J2[:, 0] = np.vstack(cross(nz(), c2 - p1), nz())
+        J2[:, 1] =  np.vstack(cross(ny(), c2 - p2), ny())
+
+        J3 = np.zeros((5, 3))
+        J3[:, 0] = np.vstack(cross(nz(), c3 - p1), nz())
+        J3[:, 1] =  np.vstack(cross(ny(), c3 - p2), ny())
+        J3[:, 2] = np.vstack(cross(ny(), c3 - p3), ny())
+
+        J4 = np.zeros((5, 3))
+        J4[:, 0] = np.vstack(cross(nz(), c4 - p1), nz())
+        J4[:, 1] =  np.vstack(cross(ny(), c4 - p2), ny())
+        J4[:, 2] = np.vstack(cross(ny(), c4 - p3), ny())
+        J4[:, 3] = np.vstack(cross(ny(), c4 - p4), ny())
+
+
+        J5 = np.zeros((5, 3))
+        J5[:, 0] = np.vstack(cross(nz(), c4 - p1), nz())
+        J5[:, 1] =  np.vstack(cross(ny(), c4 - p2), ny())
+        J5[:, 2] = np.vstack(cross(ny(), c4 - p3), ny())
+        J5[:, 3] = np.vstack(cross(ny(), c4 - p4), ny())
+        J5[:, 4] = np.vstack(cross(nx(), c4 - p4), nx())
+    
+        # now find the total COM jacobian by summing and dividing by the mass
+        J = (m1*J1 + m2*J2 + m3*J3 + m4*J4 + m5*J5)/(m1+m2+m3+m4+m5)
+
+        return J, J1, J2, J3, J4, J5
 
     # Evaluate at the given time.  This was last called (dt) ago.
     def evaluate(self, t, dt):
@@ -110,14 +152,14 @@ def main(args=None):
 
     # Initialize the generator node for 100Hz udpates, using the above
     # Trajectory class.
-    generator = GeneratorNode('generator', 100, Trajectory)
+    generator2 = GeneratorNode('generator2', 100, Trajectory2)
 
     # Spin, meaning keep running (taking care of the timer callbacks
     # and message passing), until interrupted or the trajectory ends.
-    generator.spin()
+    generator2.spin()
 
     # Shutdown the node and ROS.
-    generator.shutdown()
+    generator2.shutdown()
     rclpy.shutdown()
 
 if __name__ == "__main__":
